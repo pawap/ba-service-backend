43d785533b73c89cf6ee4d19dd957373804e436c
trew26 <spm@instant.de>
2019-04-17 07:00:49 +0200

    Create json_to_txt
    short parser for json to txt, not working yet

---
+++ src/main/python/json_to_txt.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import filedialog
+import json
+
+def json_to_txt():
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+
+
+    with open(file_path) as f:
+      data = json.load(f)
+
+    # Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+    print(data)
