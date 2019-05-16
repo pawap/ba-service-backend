2f21956c2792c3d5b0d79e3a59d542590f8f9772
erk4n <erkan.karabu@gmail.com>
2019-05-16 18:38:12 +0200

    Test Commit

--- src/main/java/sentiments/ApplicationController.java
+++ src/main/java/sentiments/ApplicationController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
  * Dummy App-Controller for determining the initial project architecture
  *
  */
+
 @RestController
 @EnableAutoConfiguration
 @ComponentScan
