c8699001f8100d727d2533ec469cabccfafc8980
trew26 <spm@instant.de>
2019-06-19 02:25:01 +0200

    [INIT]

---
+++ src/main/java/sentiments/MongoDBConfig.java
@@ -0,0 +1,23 @@
+package sentiments;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.sql.Timestamp;
+
+@EnableMongoRepositories(basePackageClasses = TweetRepository.class)
+@Configuration
+public class MongoDBConfig {
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(TweetRepository tweetRepository) {
+//        return strings -> {
+//            tweetRepository.save(new Tweet(1,"ein tweet",new Timestamp(100),new Timestamp(200)));
+//            tweetRepository.save(new Tweet(2,"noch ein tweet",new Timestamp(200),new Timestamp(300)));
+//        };
+//
+//       }
+}
--- src/main/java/sentiments/TweetRepository.java
+++ src/main/java/sentiments/TweetRepository.java
@@ -3,24 +3,24 @@ package sentiments;
 import java.sql.Timestamp;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author Paw
  *
  */
-public interface TweetRepository extends CrudRepository<Tweet, Integer> {
+public interface TweetRepository extends MongoRepository<Tweet, Integer> {
 	
-	@Query("from Tweet where offensive=:offensive")
-	public Iterable<Tweet> findAllByOffensive(@Param("offensive") Boolean offensive);
+//	@Query("from Tweet where offensive=:offensive")
+	public Iterable<Tweet> findAllByOffensive(Boolean offensive);
 	
-	@Query("from Tweet where offensive=:offensive and crdate>=:startdate and crdate<=:enddate")
-	public Iterable<Tweet> findAllByOffensiveAndDate(@Param("offensive") Boolean offensive, @Param("startdate") Timestamp startdate, @Param("enddate") Timestamp enddate);
+//	@Query("from Tweet where offensive=:offensive and crdate>=:startdate and crdate<=:enddate")
+//	public Iterable<Tweet> findAllByOffensiveAndDate(@Param("offensive") Boolean offensive, @Param("startdate") Timestamp startdate, @Param("enddate") Timestamp enddate);
 
-	@Query("select count(*) from Tweet where offensive=:offensive")
-	public int countByOffensive(@Param("offensive") Boolean offensive);
+//	@Query("select count(*) from Tweet where offensive=:offensive")
+//	public int countByOffensive(@Param("offensive") Boolean offensive);
 	
-	@Query("select count(*) from Tweet where offensive=:offensive and crdate>=:startdate and crdate<=:enddate")
-	public int countByOffensiveAndDate(@Param("offensive") Boolean offensive, @Param("startdate") Timestamp startdate, @Param("enddate") Timestamp enddate);
+//	@Query("select count(*) from Tweet where offensive=:offensive and crdate>=:startdate and crdate<=:enddate")
+//	public int countByOffensiveAndDate(@Param("offensive") Boolean offensive, @Param("startdate") Timestamp startdate, @Param("enddate") Timestamp enddate);
 }
